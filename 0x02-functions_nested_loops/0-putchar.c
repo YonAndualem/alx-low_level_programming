2f52d93d90aaa50b3224eec1da3044cec59e7f72
Yonas <yonasandualem1472@gmail.com>
2022-06-06 22:43:20 -0700

    Try 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+/**
+ * main - Entry block
+ * Description: prints _putchar on new line
+ * Return: 0
+ */
+int main(void)
+{
+	char c[8] = "_putchar";
+
+	int i = 0;
+
+	while (i < 7)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}

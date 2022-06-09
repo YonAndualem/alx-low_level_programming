7d8bca74a189ae6468169dc624f6e5bc6abca87f
Yonas <yonasandualem1472@gmail.com>
2022-06-09 10:25:33 -0700

    All Tasks

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * mul - returns the product of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
  */
-int main(void)
-{
 
-	return(0)
+
+int mul(int a, int b)
+{
+return (a * b);
 }
